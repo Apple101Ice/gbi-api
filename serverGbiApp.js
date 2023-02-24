f39fd40535b77bb0d020fbb654a293c0fa986350
Apple101Ice <mercenaryxx@gmail.com>
2023-02-24 11:35:32 +0530

    Another PORT fix

--- serverGbiApp.js
+++ serverGbiApp.js
@@ -10,7 +10,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
   next();
 });
-const port = process.env.PORT || 2410;
+const port = process.env.PORT;
 grtOrLess = "";
 statecity = [];
 customers = [
